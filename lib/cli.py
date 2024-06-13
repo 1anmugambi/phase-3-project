84644c0de922e7937c4ec606404ca42b6f16ba4d
Ian Mugambi <ian.mugambi1@student.moringaschool.com>
2024-06-13 21:18:45 +0300

    Update cli.py

--- lib/cli.py
+++ lib/cli.py
@@ -1,29 +1,28 @@
+import sys
+import os
+
+# Append the absolute path of the project directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from lib.helpers import add_location, view_locations, exit_program
 
-# Main function that runs the CLI
 def main():
     while True:
-        # Display the menu and get user input
-        menu()
+        print("Please select an option:")
+        print("0. Exit the program")
+        print("1. Add a location")
+        print("2. View locations")
         choice = input("> ")
 
-        # Execute functions based on user input
         if choice == "0":
             exit_program()
+            break
         elif choice == "1":
             add_location()
         elif choice == "2":
             view_locations()
         else:
-            print("Invalid choice")
+            print("Invalid choice. Please try again.")
 
-# Function to display the menu options
-def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Add a location")
-    print("2. View locations")
-
-# Entry point of the program
 if __name__ == "__main__":
     main()
